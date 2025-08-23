e0e8ff1195bc124ae7b5e6895fa9088926396a1f
Aashish@2221 <patilaashish2221@gmail.com>
2025-08-23 18:05:48 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/authController');
 const authenticateToken = require('../middleware/authMiddleware');
-const { getCustomerInfo, updateCustomerInfo } = require('../controllers/customerController');
+const { getCustomerInfo, updateCustomerInfo } = require('../controllers/CustomerInfoController');
 
 router.post('/register', register);
 router.post('/login', login);
