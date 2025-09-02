b7dca2436d7a2d988647c074f980c53d67cf1c79
Aashish2221 <135332381+Aashish2221@users.noreply.github.com>
2025-09-02 09:52:25 +0530

    .

--- models/CustomerInfo.js
+++ models/CustomerInfo.js
@@ -2,16 +2,14 @@ const mongoose = require('mongoose');
 
      const customerInfoSchema = new mongoose.Schema({
        userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, unique: true },
-       lastName: { type: String },
-       firstName: { type: String },
-       emailId: { type: String }, // No unique index
-       mobNo: { type: String },
-       phone: { type: String },
-       city: { type: String },
-       pincode: { type: String },
-       state: { type: String },
+       name: { type: String, required: true },
+       emailId: { type: String, required: true }, // No unique index
+       mobNo: { type: String, required: true },
+       city: { type: String, required: true },
+       pincode: { type: String, required: true },
+       state: { type: String, required: true },
        profilePhoto: { type: String },
-       address: { type: String },
+       address: { type: String, required: true },
        createdAt: { type: Date, default: Date.now },
        updatedAt: { type: Date, default: Date.now }
      });
